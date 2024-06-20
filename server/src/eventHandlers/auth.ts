dbb010b7c4c61dd39a6e6f28ee37862e3944db4b
Siddharth Singh Rajvanshi <siddharth.s@intimetec.com>
2024-06-20 18:45:15 +0530

    Admin operations

---
+++ server/src/eventHandlers/auth.ts
@@ -0,0 +1,21 @@
+import { Socket } from "socket.io";
+
+import AuthSocketHandler from "../socketHandlers/auth";
+
+const authSocketHandler = new AuthSocketHandler();
+
+
+export default class AuthEventHandler {
+    socket
+
+    constructor(socket: Socket) {
+        this.socket = socket
+    }
+
+    listen(){
+        this.socket.on("login", async (data) => {
+            await authSocketHandler.login(this.socket, data);
+        });
+    }
+}
+
---
+++ server/src/eventHandlers/menuItem.ts
@@ -0,0 +1,35 @@
+import { Socket } from "socket.io";
+import MenuItemSocketHandler from "../socketHandlers/menuItem";
+
+const menuItemSocketHandler = new MenuItemSocketHandler();
+
+
+export default class MenuItemEventHandler {
+    socket
+
+    constructor(socket: Socket) {
+        this.socket = socket
+    }
+
+    listen(){
+        this.socket.on("createMenuItem", async (data) => {
+            await menuItemSocketHandler.createMenuItem(this.socket, data);
+        });
+        this.socket.on("getMenuItems", async () => {
+            await menuItemSocketHandler.getMenuItems(this.socket);
+        });
+        this.socket.on("getMenuItemById", async (data) => {
+            await menuItemSocketHandler.getMenuItemById(this.socket, data);
+        });
+        this.socket.on("updateMenuItem", async (data) => {
+            await menuItemSocketHandler.updateMenuItem(this.socket, data);
+        });
+        this.socket.on("deleteMenuItem", async (data) => {
+            await menuItemSocketHandler.deleteMenuItem(this.socket, data);
+        });
+        this.socket.on("updateMenuItemAvailability", async (data) => {
+            await menuItemSocketHandler.updateMenuItemAvailability(this.socket, data);
+        });
+    }
+}
+
